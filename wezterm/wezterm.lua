a241d448a321fdd9ad155bf6820e3903827c63bb
Aleksey Sakovets <alexeysakovets@gmail.com>
2026-03-25 21:10:37 +0300

    wezterm.lua

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,64 @@
+local wezterm = require("wezterm")
+local act = wezterm.action
+local config = wezterm.config_builder()
+
+local IS_MAC = wezterm.target_triple:find("darwin") ~= nil
+local IS_WINDOWS = wezterm.target_triple:find("windows") ~= nil
+local IS_LINUX = wezterm.target_triple:find("linux") ~= nil
+
+config.enable_scroll_bar = true
+config.enable_tab_bar = true
+config.hide_tab_bar_if_only_one_tab = true
+config.quick_select_remove_styling = true
+config.warn_about_missing_glyphs = false
+
+-- config.window_background_opacity = 0.85
+-- config.font_size = 13
+-- config.font = wezterm.font("jetbrains mono", { weight = "Regular" })
+
+local f,err = loadfile(wezterm.config_dir.."/wezterm-local.lua")
+if err then
+    wezterm.log_error(err)
+else
+    f().apply_to_config(config)
+end
+
+config.leader = { key = ",", mods = "CTRL" }
+config.keys = {
+    { key = "{", mods = "SHIFT|ALT", action = act.ActivateTabRelative(-1) },
+    { key = "}", mods = "SHIFT|ALT", action = act.ActivateTabRelative(1) },
+    { key = "n", mods = "LEADER", action = act.SpawnTab("CurrentPaneDomain") },
+    { key = ",", mods = "CTRL|LEADER", action = act.SendKey({ key = ",", mods = "CTRL" }) },
+
+    --= Panes
+    { key = "b", mods = "LEADER", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "v", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "z", mods = "LEADER", action = act.TogglePaneZoomState },
+    -- Navigate panes
+    { key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+    { key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
+    { key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+    { key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+      -- Resize pane
+    { key = "LeftArrow",  mods = "CTRL|ALT", action = act.AdjustPaneSize {"Left", 3}},
+    { key = "RightArrow", mods = "CTRL|ALT", action = act.AdjustPaneSize {"Right", 3}},
+    { key = "UpArrow",    mods = "CTRL|ALT", action = act.AdjustPaneSize {"Up", 3}},
+    { key = "DownArrow",  mods = "CTRL|ALT", action = act.AdjustPaneSize {"Down", 3}},
+}
+
+for i = 1, 9 do
+    table.insert(config.keys, { key = tostring(i), mods = "ALT", action = act.ActivateTab(i - 1) })
+end
+
+config.mouse_bindings = {
+    {
+        event = { Down = { streak = 1, button = { WheelUp = 1 } } },
+        action = act.ScrollByLine(-3),
+    },
+    {
+        event = { Down = { streak = 1, button = { WheelDown = 1 } } },
+        action = act.ScrollByLine(3),
+    },
+}
+
+return config
