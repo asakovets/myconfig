38bfac64ab32a2f0d4581f1952a4e53438ee066f
Aleksey Sakovets <alexeysakovets@gmail.com>
2026-03-29 16:45:13 +0300

    deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,29 +3,40 @@ set -e
 repodir=$(realpath $(dirname "$BASH_SOURCE"))
 echo "repodir ${repodir}"
 
+linuxp() {
+	[[ $OSTYPE = "linux-gnu" ]] || false
+}
+
+macp() {
+	[[ $OSTYPE == "darwin"* ]] || false
+}
+
+winp() {
+	[[ $OSTYPE = "msys" ]] || false
+}
+
 if [[ "$#" -eq 1 && "$1" == "-n" ]]; then
-    dolink ()
-    {
-        echo "$2 -> ${repodir}/$1"
-    }
+	dolink() {
+		echo "$2 -> ${repodir}/$1"
+	}
 else
-    dolink ()
-    {
-        echo "$2 -> ${repodir}/$1"
-        ln -sf "${repodir}/$1" "$2"
-    }
+	dolink() {
+		echo "$2 -> ${repodir}/$1"
+		ln -sf "${repodir}/$1" "$2"
+	}
 
 fi
 
-ens ()
-{
-    mkdir -p "$1"
+ens() {
+	mkdir -p "$1"
 }
 
-ens ~/.config/fish && \
-    dolink ~/.config/fish/fish.config fish/fish.config
+(macp || linuxp) &&
+	ens ~/.config/fish &&
+	dolink ~/.config/fish/fish.config fish/fish.config
 
-ens ~/.config/wezterm && \
-    dolink ~/.config/wezterm/wezterm.lua wezterm/wezterm.lua
+ens ~/.config/wezterm &&
+	dolink ~/.config/wezterm/wezterm.lua wezterm/wezterm.lua
 
-dolink ~/.tmux.conf tmux/tmux.conf
+(macp || linuxp) &&
+	dolink ~/.tmux.conf tmux/tmux.conf
